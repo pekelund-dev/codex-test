9c44738d70bcbe75c4b5b2edc3b5af09df4dbad0
pekelund-dev <155527227+pekelund-dev@users.noreply.github.com>
2025-11-25 15:05:47 +0100

    Fix receipt parser test dependencies and retry version

--- receipt-parser/src/test/java/dev/pekelund/pklnd/receiptparser/ReceiptProcessingControllerTest.java
+++ receipt-parser/src/test/java/dev/pekelund/pklnd/receiptparser/ReceiptProcessingControllerTest.java
@@ -4,25 +4,34 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest(ReceiptProcessingController.class)
+@ExtendWith(MockitoExtension.class)
 class ReceiptProcessingControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @Mock
     private ReceiptParsingHandler receiptParsingHandler;
+
+    private ReceiptProcessingController controller;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+        controller = new ReceiptProcessingController(objectMapper, receiptParsingHandler);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
 
     @Test
     void delegatesToHandlerWhenPayloadValid() throws Exception {
